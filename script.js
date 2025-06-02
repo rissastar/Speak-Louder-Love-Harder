13a8db7ad83448ebabb60567944bec79181c0d70
rissastar <larissastar0316@gmail.com>
2025-06-02 14:22:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -128,4 +128,9 @@
     fadeInOnScroll();
     updateProgressBar();
   });
+  const toggleBtn = document.getElementById('dark-mode-toggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  toggleBtn.textContent = document.body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
+});
 </script>
