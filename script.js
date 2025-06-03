07c5340f90b2a837fc62423888aa96a617ccf7ca
rissastar <larissastar0316@gmail.com>
2025-06-03 03:43:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -179,4 +179,20 @@ if (progressBar) {
     progressBar.style.width = `${scrollPercent}%`;
   });
 }
+
+  document.querySelectorAll(".collapsible").forEach(button => {
+    button.addEventListener("click", function () {
+      this.classList.toggle("active");
+      const content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+        content.style.paddingTop = "0";
+        content.style.paddingBottom = "0";
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        content.style.paddingTop = "15px";
+        content.style.paddingBottom = "15px";
+      }
+    });
+  });
 });
