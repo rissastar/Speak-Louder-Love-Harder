d17d8b243f43702b175929562fb898041ab88bc3
rissastar <larissastar0316@gmail.com>
2025-06-17 02:31:34 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,5 +0,0 @@
-// supabase.js
-const SUPABASE_URL = 'https://ytgrzhtntwzefwjmhgjj.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0Z3J6aHRudHd6ZWZ3am1oZ2pqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2MTA4NjYsImV4cCI6MjA2NTE4Njg2Nn0.wx89qV1s1jePtZhuP5hnViu1KfPjMCnNrtUBW4bdbL8';
-
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
